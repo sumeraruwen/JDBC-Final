7b5c6b3b8aefa2c797470291b116d70736b5365e
unknown <sumeraruwen@gmail.com>
2022-07-12 10:05:48 +0530

    Create UI

--- src/controller/ManageStudentFormController.java
+++ src/controller/ManageStudentFormController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
 public class ManageStudentFormController {
+    public JFXButton btnAddNewStudent;
+    public JFXTextField txtStudentId;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtEmail;
+    public JFXButton btnSave;
+    public JFXButton btnDelete;
+    public TableView tblStudents;
+
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
 }
